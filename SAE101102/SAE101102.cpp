0020866d31e82adec42be3a46c5ce9cf77a48174
A-Redox <redoxpro82@gmail.com>
2022-01-14 15:41:54 +0100

    V1

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World!\n";
-    cout << "Welcome to SAE 101 and 102 !\n";
+
+
+// Taille maximale de la bambouseraie
+const int TAILLE_MAX = 100;
+
+// Definition des bambou
+struct Bambou {
+    int croissance;
+    int taille;
+};
+
+int VerifMax(Bambou Bambou, int TailleChoisie) {
+
+    int TailleMax = 0;
+    int indiceBambou;
+
+    for (int i = 0; i < TailleChoisie; i++) {
+        if (Bambou.taille > TailleMax) {
+            TailleMax = Bambou.taille;
+            indiceBambou = i;
+        }
+    }
+    return indiceBambou;
 }
 
+int main() {
+    Bambou bambouseraie[TAILLE_MAX];
+}
