b537a32b54ba3f51c8d08a88d11872bfadf35312
Cryo Delta <Razmomail01@gmail.com>
2022-01-14 16:19:28 +0100

    GrowAll

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,3 +1,6 @@
-#include <iostream>
-using namespace std;
-
+// Croissance simultané de la bambouraie
+void GrowAll(Bambou bambouraie[], int taille) {
+    for (int i = 0; i < taille; i++) {
+        GrowBambou(bambouraie[i]);
+    }
+}
