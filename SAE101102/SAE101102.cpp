b296419385800b3e7893420dd2b0ad460931e9d9
Cryo Delta <Razmomail01@gmail.com>
2022-01-14 16:37:28 +0100

    ;

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -49,7 +49,7 @@ void ReduceMax(Bambou bambou, int tab[], int TailleChoisie) {
 int ReduceFaster(Bambou bambou, int seuil, Bambou tab[], int TailleChoisie) {
     int maxCroissance = 0;
     if (bambou.taille = seuil) {
-        for (int i = 0; i < TailleChoisie, i++) {
+        for (int i = 0; i < TailleChoisie; i++) {
             if (tab[i].croissance > maxCroissance) {
                 maxCroissance = tab[i].croissance;
             }
