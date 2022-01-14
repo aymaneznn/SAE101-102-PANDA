39abbf9d4789098d81c7b723ac1b45ebe5278dff
Aymane Zennouhi <aymanezennouhi01@gmail.com>
2022-01-14 15:57:37 +0100

    Fonction ReduceMax, CutBambou términée

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -7,25 +7,50 @@ using namespace std;
 const int TAILLE_MAX = 100;
 
 // Definition des bambou
+
 struct Bambou {
     int croissance;
     int taille;
 };
 
-void VerifMax(Bambou Bomboo, int TailleChoisie) {{
-        
+
+
+// Fonction qui chrecher la bambou le plus grand
+
+int VerifMax(Bambou bambou, int TailleChoisie) {
+    
+
         int TailleMax = 0;
         int indiceBambou;
 
-        for (int i = 0 ; i < TailleChoisie; i++) {
-            if (Bamboo.Taille > TailleMax) {
-                TailleMax = Bamboo.Taille;
+        for (int i = 0; i < TailleChoisie; i++) {
+            if (bambou.taille > TailleMax) {
+                TailleMax = bambou.taille;
                 indiceBambou = i;
             }
         }
 
+        return indiceBambou;
+}
+
+// Fonction qui enleve le bambou  Reduce - Max()
+
+void ReduceMax(Bambou bambou, int tab[], int TailleChoisie) {
+    int taille;
+    int indice = VerifMax(bambou, TailleChoisie);
+    tab[indice] = bambou.croissance;
+}
+
+
+// Fonction qui permet d'effacer le bambou le plus grand en laissant sa taille = croissance
+
+void CutBambou(Bambou bambou) {
+
+    bambou.taille = bambou.croissance;
 
 }
+
+// Fonction 
 
 int main(){
     Bambou bambouseraie[TAILLE_MAX];
