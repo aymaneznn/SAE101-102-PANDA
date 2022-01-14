41af167fee2078d6056456d1a6c2935899d6a659
Aymane Zennouhi <aymanezennouhi01@gmail.com>
2022-01-14 15:23:05 +0100

    Fonction VerifMax faite

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+// Reduce - Max()
+
+void VerifMax(Bambou Bomboo, int TailleChoisie) {{
+        
+        int TailleMax = 0;
+        int indiceBambou;
+
+        for (int i = 0 ; i < TailleChoisie; i++) {
+            if (Bamboo.Taille > TailleMax) {
+                TailleMax = Bamboo.Taille;
+                indiceBambou = i;
+            }
+        }
+
+
+}
+
+
 int main(){
     cout << "Hello World!\n";
 }
