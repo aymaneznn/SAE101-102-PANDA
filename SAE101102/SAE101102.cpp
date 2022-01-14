453754335c11904ea696993c51f51771793f7395
Aymane Zennouhi <aymanezennouhi01@gmail.com>
2022-01-14 16:19:25 +0100

    Fonction ReduceFaster términée

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,57 +1,16 @@
-#include <iostream>
-using namespace std;
 
+// Fonction Reduce-Fastest(x)
 
-
-// Taille maximale de la bambouseraie
-const int TAILLE_MAX = 100;
-
-// Definition des bambou
-
-struct Bambou {
-    int croissance;
-    int taille;
-};
-
-
-
-// Fonction qui chrecher la bambou le plus grand
-
-int VerifMax(Bambou bambou, int TailleChoisie) {
-    
-
-        int TailleMax = 0;
-        int indiceBambou;
-
-        for (int i = 0; i < TailleChoisie; i++) {
-            if (bambou.taille > TailleMax) {
-                TailleMax = bambou.taille;
-                indiceBambou = i;
+int ReduceFaster(Bambou bambou, int seuil, Bambou tab[], int TailleChoisie) {
+    int maxCroissance = 0;
+    if (bambou.taille = seuil) {
+        for (int i = 0; i < TailleChoisie, i++) {
+            if (tab[i].croissance > maxCroissance) {
+                maxCroissance = tab[i].croissance;
             }
         }
-
-        return indiceBambou;
+       
+    }
+    return maxCroissance;
 }
 
-// Fonction qui enleve le bambou  Reduce - Max()
-
-void ReduceMax(Bambou bambou, int tab[], int TailleChoisie) {
-    int taille;
-    int indice = VerifMax(bambou, TailleChoisie);
-    tab[indice] = bambou.croissance;
-}
-
-
-// Fonction qui permet d'effacer le bambou le plus grand en laissant sa taille = croissance
-
-void CutBambou(Bambou bambou) {
-
-    bambou.taille = bambou.croissance;
-
-}
-
-// Fonction 
-
-int main(){
-    Bambou bambouseraie[TAILLE_MAX];
-}
