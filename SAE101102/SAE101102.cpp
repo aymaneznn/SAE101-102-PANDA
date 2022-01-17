3ed60d2bf0ba2ac7fa0f91d150c74a95eef9e980
Cryo Delta <Razmomail01@gmail.com>
2022-01-17 09:17:09 +0100

    .

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,6 +1,19 @@
-// Initialisation de bambouraie
-void InitBamboueraie(Bambou bambouraie[], int taille) {
-	for (int i = 0; i < taille; i++) {
-		CreateBambou(bambouraie[i]);
-	}
+// Statistique de la taille moyenne de la bambouseraie
+float TailleMoy(Bambou tab[], int taille) {
+    float total = 0;
+    for (int i = 0; i < taille; i++) {
+        total += tab[i].taille;
+    }
+    return total/taille;
+}
+
+// Statistique de la taille minimum de la bambouseraie
+int TailleMin(Bambou tab[], int taille) {
+    int min = tab[0].taille;
+    for (int i = 0; i < taille; i++) {
+        if (tab[i].taille < min) {
+            min = tab[i].taille;
+        }
+    }
+    return min;
 }
