cab4002fb75018db83e8e8b6358cc79bde9ff3ac
Cryo Delta <Razmomail01@gmail.com>
2022-01-17 09:31:14 +0100

    Statistiques Moy et Min

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -1,27 +1,3 @@
-using namespace std;
-#include<iostream>
-
-const int TAILLE_MAX = 100;
-
-// Definition des bambou
-struct Bambou {
-    int croissance;
-    int taille;
-};
-
-// Création d'un bambou
-void CreateBambou(Bambou& bambou, int taille) {
-    bambou.croissance = taille;
-    bambou.taille = taille;
-}
-
-// Initialisation de bambouraie
-void InitBamboueraie(Bambou bambouraie[], int taille, int tab[]) {
-    for (int i = 0; i < taille; i++) {
-        CreateBambou(bambouraie[i], tab[i]);
-    }
-}
-
 // Statistique de la taille moyenne de la bambouseraie
 float TailleMoy(Bambou tab[], int taille) {
     float total = 0;
@@ -40,10 +16,4 @@ int TailleMin(Bambou tab[], int taille) {
         }
     }
     return min;
-}
-
-void main() {
-    Bambou bambousaie[TAILLE_MAX];
-    int taille = 10;
-    InitBamboueraie(bambousaie, taille, {})
 }
