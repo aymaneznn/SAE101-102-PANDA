23920a29af46cac43da078c5bf390689b0bc063a
Aymane Zennouhi <aymanezennouhi01@gmail.com>
2022-01-19 14:06:20 +0100

    .

--- SAE101102/SAE101102.cpp
+++ SAE101102/SAE101102.cpp
@@ -309,7 +309,7 @@ void batterie_lv1() {
     char lv1[] = "batterie_lv1.bmp";
     char lv2[] = "batterie_lv2.bmp";
     char lv3[] = "batterie_lv3.bmp";
-    char names[] = "batterie_lv3.bmp";
+
     if (indice_batterie <= 10) {
         batterie(lv3);
     }
